92500238e42e8c0277e94ff4ed2879a4e3454a67
Andrei <roman_andrei94@yahoo.com>
2016-07-15 10:52:10 +0300

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andrei/Desktop/myapp/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myapp/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import include, url
+from django.contrib import  admin
+from rest_framework import routers
+from restaurant import views
+	
+
+
+# router = routers.DefaultRouter()
+# router.register(r'api/menus', views.MenuViewSet)
+# router.register(r'api/orders', views.OrderViewSet)
+
+urlpatterns = [
+	url(r'^api/menus', views.MenuViewSet),
+    url(r'^api/orders/', views.OrderViewSet),
+    url(r'^api/info', views.InfoViewSet),
+    url(r'^api/rating', views.RaitingViewSet),
+    url(r'^admin/', admin.site.urls),
+    url(r'^restaurant/', include('restaurant.urls', namespace='restaurant')), 
+	
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))    
+]
